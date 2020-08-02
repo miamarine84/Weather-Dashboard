a5f95d033065bb45cd05f8c495f69fa1b2ce9b3e
marco p <miamarine84@yahoo.com>
2020-08-02 07:25:05 -0400

    html and css done now working on script file but stuck on userInput var

---
+++ script.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+
+$('#button').click(function(e){
+    console.log('yes');
+
+   //userInput = $('#userText').val();
+
+})
+var userInput = 'dfasfas'
+
+// $.ajax({
+//     url: "/trips",
+//     type: "post"
+//   });
+});
