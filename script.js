0dd8462360c75f5f0ce99c24e634d78bda988e3a
marco p <miamarine84@yahoo.com>
2020-08-02 21:20:23 -0400

    left off working on ajax function

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-$( document ).ready(function() {
+$(document).ready(function() {
     console.log( "ready!" );
 
 
-$('#button').click(function(e){
-    console.log('yes');
 
-   //userInput = $('#userText').val();
+$('#inputButton').click(function(e){
+    
 
+    let userInput = $('#userText').val();
+    console.log(userInput);
+    e.preventDefault();
 })
-var userInput = 'dfasfas'
+function weather(){
+let weatherURL = "api.openweathermap.org/data/2.5/weather?q=miami&appid=2a255b648d741897177dc2c2d31abf26"
 
-// $.ajax({
-//     url: "/trips",
-//     type: "post"
-//   });
+
+$.ajax({
+    url: weatherURL,
+    type: "GET"
+  }).then(function(response){
+      console.log(response)
+  });
+}
 });
