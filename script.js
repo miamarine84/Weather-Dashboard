bbe967180511c7c0def172b23e9b69e4eedabcba
marco p <miamarine84@yahoo.com>
2020-08-04 06:08:31 -0400

    started working ith object and sending it over to the webpage

--- script.js
+++ script.js
@@ -7,18 +7,28 @@ $('#inputButton').click(function(e){
     let userInput = $('#userText').val();
     e.preventDefault();
     weather(userInput);
+
+
 })
+
+
+
 
 function weather(userInput){
 
-let weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=2a255b648d741897177dc2c2d31abf26";
+let weatherURL = 
+"https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&appid=2a255b648d741897177dc2c2d31abf26"
+
 
 
 $.ajax({
     url: weatherURL,
     type: "GET"
   }).then(function(response){
-      console.log(response)
+    console.log(response)  
+    console.log(response.city)
+    let date = $('<h4>').text(response.list[7].dt_txt);
+    $('#weather-ajax').append(date);
   });
 }
 
